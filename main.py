b7a28a702df7bb5a7bbec073f4966584e71fe36d
omshii <omshisamal72@gmail.com>
2020-07-10 18:48:58 +0530

    kinda works

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from bs4 import BeautifulSoup
 import numpy as np
 import sys
 import pandas
-from row import Row
-from wikitable import Wikitable
+from table import Wikitable
 
 url = sys.argv[1]
 
@@ -15,8 +14,21 @@ tables_list=soup.find_all('table', {'class': 'wikitable'})
 
 for table in tables_list:
     rows = table.find_all('tr')
-    for row in rows:\
-        cells = row.find_all('td', 'th')
+    table = Wikitable()
+    for row in rows:
+        cells = row.find_all(['td', 'th'])
+        if cells is None:
+            continue
         for cell in cells:
             rowspan = cell.get('rowspan')
-            colspan = cell.get('colspan')\
+            colspan = cell.get('colspan')
+            if colspan is None:
+                colspan = 1
+            if rowspan is None:
+                rowspan = 1
+            for i in range(int(colspan)):
+                table.add_cell(cell.text.strip())
+            for i in range(int(rowspan)-1):
+                table.next_add(cell.text.strip(), i+1)
+        table.iterate_row()
+    break
