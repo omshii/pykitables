f69a77275bd4d78e5528e1261a44a58b56a66691
omshii <omshisamal72@gmail.com>
2020-07-13 00:16:47 +0530

    package setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pykitables",
+    version="0.0.1",
+    author="Omshi Samal",
+    author_email="omshisamal72@gmail.com",
+    description="Get neatly formatted tables from any Wikipedia article",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/omshii/pykitables",
+    install_requires=['bs4'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
